a5454fb489d951e5e1d9af2dcfb69875ce810038
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-03-05 01:15:31 +0200

    Оновлює 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,18 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 const onPlay = function (data) {
-  localStorage.setItem(
-    'videoplayer-current-time',
-    JSON.stringify(data.seconds)
-  );
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data.seconds));
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
   })
