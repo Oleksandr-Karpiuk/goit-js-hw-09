a609ee5333cb4875a91f32bd162b9c0215860dfa
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-03-09 11:02:59 +0200

    Оновлює 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+/*
+HTML містить кнопки «Start» і «Stop».
 
+<button type="button" data-start>Start</button>
+<button type="button" data-stop>Stop</button>
+
+Напиши скрипт, який після натискання кнопки «Start»,
+раз на секунду змінює колір фону <body> на випадкове значення,
+використовуючи інлайн стиль. Натисканням на кнопку «Stop»
+зміна кольору фону повинна зупинятися.
+
+УВАГА
+Враховуй, що на кнопку «Start» можна натиснути нескінченну кількість разів.
+Зроби так, щоб доки зміна теми запущена, кнопка «Start» була неактивною (disabled).
+
+Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+*/
+
+/* ПРИКЛАД ЗМІНИ КОЛЬОРУ BODY ПО КНОПЦІ.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const buttonChangeColorRef = document.querySelector(".change-color");
+const colorDescriptionRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+
+buttonChangeColorRef.addEventListener("click", onButtonChangeColorClick);
+
+function onButtonChangeColorClick() {
+  let randomColor = getRandomHexColor();
+  colorDescriptionRef.textContent = randomColor;
+  bodyRef.style.backgroundColor = randomColor;
+}
+*/
