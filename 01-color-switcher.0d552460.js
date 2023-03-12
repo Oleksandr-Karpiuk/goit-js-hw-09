6de51a0349629828049934c95ee622a7e847e9e8
Oleksandr-Karpiuk <113094326+Oleksandr-Karpiuk@users.noreply.github.com>
2023-03-12 12:41:47 +0000

    Deploying to gh-pages from @ Oleksandr-Karpiuk/goit-js-hw-09@3c3fada597a73e0254a3633e2d9c5c05afc5bb55 🚀

---
+++ 01-color-switcher.0d552460.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),n=document.querySelector("body"),d=null;function a(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a(),d=setInterval((function(){return a()}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.0d552460.js.map
